5f03596b03f3914e5cf7c98d147dadb08e943293
Magnus Hörberg <magnus.hoerberg@gmail.com>
2016-09-29 09:13:29 +0200

    generate one json messsage per query run

--- jmxtrans-output/jmxtrans-output-rabbitmq/src/main/java/com/googlecode/jmxtrans/model/output/RabbitMQWriter.java
+++ jmxtrans-output/jmxtrans-output-rabbitmq/src/main/java/com/googlecode/jmxtrans/model/output/RabbitMQWriter.java
@@ -24,6 +24,9 @@ package com.googlecode.jmxtrans.model.output;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+import static com.fasterxml.jackson.core.JsonEncoding.UTF8;
 import com.google.common.collect.ImmutableList;
 
 import com.googlecode.jmxtrans.model.OutputWriter;
@@ -31,11 +34,14 @@ import com.googlecode.jmxtrans.model.ValidationException;
 import com.googlecode.jmxtrans.model.Query;
 import com.googlecode.jmxtrans.model.Result;
 import com.googlecode.jmxtrans.model.Server;
+import static com.googlecode.jmxtrans.model.naming.KeyUtils.getKeyString;
+import static com.googlecode.jmxtrans.model.naming.StringUtils.cleanupStr;
 
 import javax.annotation.Nonnull;
 import javax.annotation.concurrent.ThreadSafe;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.io.ByteArrayOutputStream;
 
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
@@ -54,25 +60,27 @@ import org.slf4j.LoggerFactory;
 public class RabbitMQWriter extends BaseOutputWriter {
     private static final Logger log = LoggerFactory.getLogger(RabbitMQWriter.class);
 
+    private static final String DEFAULT_ROOT_PREFIX = "servers";
+
+    private final String rootPrefix ;
     private final String uri ;
+    private final ImmutableList<String> typeNames ;
+    private final JsonFactory jsonFactory;
 
     @JsonCreator
     public RabbitMQWriter(
             @JsonProperty("uri") String uri,
+            @JsonProperty("rootPrefix") String rootPrefix,
             @JsonProperty("typeNames") ImmutableList<String> typeNames,
             @JsonProperty("booleanAsNumber") boolean booleanAsNumber,
             @JsonProperty("settings") Map<String, Object> settings,
             @JsonProperty("debug") boolean debug) {
         super(typeNames, booleanAsNumber, debug, settings);
+        this.typeNames = typeNames;
         this.uri = uri;
+        this.jsonFactory = new JsonFactory();
+        this.rootPrefix = firstNonNull(rootPrefix, DEFAULT_ROOT_PREFIX, "");
             }
-
-    private Channel createChannel() throws Exception {
-        ConnectionFactory cf = new ConnectionFactory();
-        cf.setUri(this.uri);
-        Connection connection = cf.newConnection();
-        return connection.createChannel();
-    }
 
     @Override
     public void validateSetup(Server server, Query query) throws ValidationException {}
@@ -85,15 +93,47 @@ public class RabbitMQWriter extends BaseOutputWriter {
         for (Result result : results) {
             Map<String, Object> resultValues = result.getValues();
             for (Map.Entry<String, Object> values : resultValues.entrySet()) {
-                Object value = values.getValue();
-                String rk = "";
-                if (result.getAttributeName().equals(values.getKey())) {
-                    rk = result.getAttributeName();
-                } else {
-                    rk = result.getAttributeName() + "_" + values.getKey();
-                }
-                pubChan.basicPublish("amq.topic", rk, null, value.toString().getBytes());
             }
+        }
+        String msg = createJsonMessage(query, results);
+        String rk = server.getAlias() + "." + query.getResultAlias();
+        pubChan.basicPublish("amq.topic", rk, null, msg.getBytes());
+    }
+
+    private Channel createChannel() throws Exception {
+        ConnectionFactory cf = new ConnectionFactory();
+        cf.setUri(this.uri);
+        Connection connection = cf.newConnection();
+        return connection.createChannel();
+    }
+
+    private String createJsonMessage(Query query, ImmutableList<Result> results) throws Exception {
+        try (
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                JsonGenerator generator = jsonFactory.createGenerator(out, UTF8)
+            )
+        {
+            generator.writeStartObject();
+            String prevName = "";
+            for (Result result : results) {
+                Map<String, Object> resultValues = result.getValues();
+                String name = query.makeTypeNameValueString(this.typeNames, result.getTypeName());
+                if (!prevName.equals(name)) {
+                    if (!prevName.equals("")) {
+                        generator.writeEndObject();
+                    }
+                    generator.writeObjectFieldStart(name);
+                }
+                for (Map.Entry<String, Object> entry : resultValues.entrySet()) {
+                    Object value = entry.getValue();
+                    generator.writeStringField(entry.getKey(), value.toString());
+                }
+                log.info(prevName, name);
+                prevName = name;
+            }
+            generator.writeEndObject();
+            generator.close();
+            return out.toString("UTF-8");
         }
     }
 }
